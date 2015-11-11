3d37d38ec5dc8ccd14f5de2f957e05aca23e6e9e
Biniam <george.beng@gmail.com>
2015-11-12 01:08:42 +0300

    test crypt method

--- Test.php
+++ Test.php
@@ -5,6 +5,8 @@ $MyDB = new DataBase();
 //$res = $MyDB->add_new_user("ben","ben","ben@ben","091641","1","Male","Ethiopia","c:---","1");
 $res = $MyDB->get_profile_("ben","ben");
 
+print_r(crypt("ben"));
+
 print_r($res);
 //print_r($MyDB->add_new_user('sds','sdsd','midd','dis','BENGEOS','phone','coun','BENGEOS','1','picture'))
 /*$val = $MyDB->get_user('BENGEOS','BENGEOS')['result'];
