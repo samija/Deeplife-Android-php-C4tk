d81f7b988f91d0f2698e54758296f1261078ff25
Biniam <george.beng@gmail.com>
2015-11-14 22:06:03 +0300

    some changes

--- Test.php
+++ Test.php
@@ -1,12 +1,12 @@
 <?php
-require_once 'Bcrypt.php';
+//require_once 'Bcrypt.php';
 require_once 'DataBase.php';
 $MyDB = new DataBase();
 //$res = $MyDB->add_new_user("ben","ben","ben@ben","091641","1","Male","Ethiopia","c:---","1");
 
-$Crypt = new \Zend\Crypt\Password\Bcrypt();
+//$Crypt = new \Zend\Crypt\Password\Bcrypt();
 
-print_r($Crypt->create($_POST['Text']));
+//print_r($Crypt->create($_POST['Text']));
 ?>
 <form method="POST" action="">
     Text<input type="text" name="Text" value="ben@ben"><br>
